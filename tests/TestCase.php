feb7dd6bf10749d458fb7721865ede24b51e46a8
Simon-bee <simonbee1303@gmail.com>
2021-02-05 11:15:23 +0100

    Tests config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    protected function signIn(?User $user = null)
+    {
+        $user = $user ?? User::factory()->create();
+
+        $this->actingAs($user);
+
+        return $user;
+    }
 }

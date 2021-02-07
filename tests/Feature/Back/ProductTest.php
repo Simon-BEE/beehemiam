3f34aa6dd75562a949c3bcc053619d997571bd69
Simon-bee <simonbee1303@gmail.com>
2021-02-07 17:13:50 +0100

    Test workflow 1

--- tests/Feature/Back/ProductTest.php
+++ tests/Feature/Back/ProductTest.php
@@ -8,36 +8,36 @@ use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
-    /** @test */
-    public function an_admin_can_see_create_product_form()
-    {
-        $user = User::factory()->create([
-            'role' => User::ADMIN_ROLE,
-        ]);
-        $this->signIn($user);
+    // /** @test */
+    // public function an_admin_can_see_create_product_form()
+    // {
+    //     $user = User::factory()->create([
+    //         'role' => User::ADMIN_ROLE,
+    //     ]);
+    //     $this->signIn($user);
 
-        $this->get(route('admin.products.create'))->assertSuccessful()
-            ->assertViewIs('admin.products.create')
-            ->assertSee('Ajouter un nouveau produit')
-        ;
-    }
+    //     $this->get(route('admin.products.create'))->assertSuccessful()
+    //         ->assertViewIs('admin.products.create')
+    //         ->assertSee('Ajouter un nouveau produit')
+    //     ;
+    // }
 
-    /** @test */
-    public function a_product_can_be_created()
-    {
-        $user = User::factory()->create([
-            'role' => User::ADMIN_ROLE,
-        ]);
-        $this->signIn($user);
+    // /** @test */
+    // public function a_product_can_be_created()
+    // {
+    //     $user = User::factory()->create([
+    //         'role' => User::ADMIN_ROLE,
+    //     ]);
+    //     $this->signIn($user);
 
-        $this->post(route('admin.products.store'), [
-            'name' => 'Mon premier produit',
-            'is_preorder' => 0,
-            'is_active' => 1,
-        ])
-            ->assertSuccessful();
+    //     $this->post(route('admin.products.store'), [
+    //         'name' => 'Mon premier produit',
+    //         'is_preorder' => 0,
+    //         'is_active' => 1,
+    //     ])
+    //         ->assertSuccessful();
 
-        $this->assertCount(1, Product::all());
-    }
+    //     $this->assertCount(1, Product::all());
+    // }
     
 }

ce8dd98c91d0f8b4dc88ed2ceed40a547cbb01ef
Simon-bee <simonbee1303@gmail.com>
2021-02-10 17:18:29 +0100

    Scripts wysiwyg in include view

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -23,7 +23,7 @@
         </h2>
     
         <div class="px-4 py-3 mb-20 bg-white rounded-lg shadow-md dark:bg-gray-800">
-            <x-form.form action="{{ route('admin.categories.update', $category) }}" method="POST" files>
+            <x-form.form action="{{ route('admin.categories.update', $category) }}" method="PATCH" files>
                 <x-back.form.input 
                     name="name"
                     type="text"
@@ -54,71 +54,5 @@
 @endsection
 
 @push('scripts')
-    <script>
-        const editor = document.querySelector('.content-editor');
-        if (editor) {
-            const editorButtons = document.querySelectorAll('.editor-button');
-            const showViews = document.querySelectorAll('.show-view');
-            // const htmlView = document.querySelector('.html-view');
-
-            checkListElements();
-
-            editorButtons.forEach((button) => {
-                button.addEventListener('click', function(e) {
-                    const action = this.getAttribute('data-action');
-
-                    switch (action) {
-                    case 'code':
-                        execCodeAction(e.currentTarget, e.currentTarget.parentNode.parentNode.parentNode.parentNode);
-                        break;
-                    case 'createLink':
-                        execLinkAction();
-                        break;
-                    default:
-                        execDefaultAction(action);
-                        break;
-                    }
-                });
-            });
-
-            function execDefaultAction(action) {
-                document.execCommand(action, false, null);
-                checkListElements();
-            }
-
-            function execLinkAction() {
-                let linkValue = prompt('URL (e.g. https://google.com/)');
-                document.execCommand('createLink', false, linkValue);
-                document.getSelection().focusNode.parentNode.classList.add("text-green-500", 'hover:underline');
-            }
-
-            function execCodeAction(button, editor){
-                
-                let htmlView = editor.querySelector('.html-view');
-                let showView = editor.querySelector('.show-view');
-                if (htmlView.classList.contains('hidden')) {
-                    htmlView.classList.replace('hidden', 'block');
-                    showView.classList.replace('block', 'hidden');
-                } else {
-                    showView.classList.replace('hidden', 'block');
-                    htmlView.classList.replace('block', 'hidden');
-                }
-
-                button.classList.toggle('bg-gray-200');
-            }
-
-            showViews.forEach(showView => showView.addEventListener('input', () => {
-                let htmlView = showView.parentNode.querySelector('.html-view');
-                htmlView.innerHTML = showView.innerHTML;
-            }));
-
-            function checkListElements() {
-                showViews.forEach(showView => showView.querySelectorAll('ul').forEach(list => {
-                    if (!list.classList.contains('list-disc')) {
-                        list.classList.add('p-2', 'bg-gray-100', 'my-2', 'list-disc', 'list-inside', 'dark:bg-gray-900', 'rounded');
-                    }
-                }));
-            }
-        }
-    </script>
+    @include('includes.scripts.wysiwyg')
 @endpush

88183ad642c21e4bf1dd7d9150e37d488c198091
Simon-bee <simonbee1303@gmail.com>
2021-02-21 12:36:01 +0100

    Register queue keep processing command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\EnsureQueueWorkerIsRunning;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        EnsureQueueWorkerIsRunning::class,
     ];
 
     /**
@@ -25,6 +26,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('personal-data-export:clean')->daily();
+
+        $schedule->command('queue:checkup')->everyFiveMinutes();
     }
 
     /**

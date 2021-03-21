55459a339be58ba457db591ba528e342507d40dd
Simon-BEE <simonbee1303@gmail.com>
2021-03-21 13:17:32 +0100

    Improve admin dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 
 class DashboardController extends Controller
 {
     public function __invoke(): View
     {
-        return view('admin.dashboard');
+        return view('admin.dashboard', [
+            'clients_count' => User::clients()->count(),
+            'orders_count' => Order::processed()->count(),
+            'preorders_count' => Order::preOrders()->count(),
+            'total_incomes' => number_format(Order::select(['id', 'price'])->processed()->get()->sum('price') / 100, 2),
+            'stock_products' => Product::lowStock()->take(5),
+            'latest_orders' => Order::latest()->withCount('orderItems')->take(5)->get(),
+            'latest_users' => User::clients()->latest()->take(5)->get(),
+        ]);
     }
 }

d2f97df8a6487c3a2b23e71db8205625c5617bdf
Simon-BEE <simonbee1303@gmail.com>
2021-03-23 10:13:33 +0100

    Filter simple order

--- app/Http/Controllers/Admin/Order/IndexOrderController.php
+++ app/Http/Controllers/Admin/Order/IndexOrderController.php
@@ -3,18 +3,43 @@
 namespace App\Http\Controllers\Admin\Order;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\FilterOrderRequest;
 use App\Models\Order;
+use App\Models\OrderStatus;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 
 class IndexOrderController extends Controller
 {
-    public function __invoke(): View
+    public function __invoke(FilterOrderRequest $request): View
     {
+        $orders = Order::query();
+
+        if ($request->get('order_id') || $request->get('order_status_id')) {
+            $orders = $this->filterOrders($orders);
+        }
+
         return view('admin.orders.index', [
-            'orders' => Order::with(['status'])
+            'orders' => $orders->with(['status'])
                 ->withCount('orderItems')
                 ->latest()
                 ->paginate(),
+            'status' => OrderStatus::all(),
         ]);
+    }
+
+    private function filterOrders(Builder|Model $orders): Builder
+    {
+        $orders->when(request()->get('order_id'), function ($query) {
+                    return $query->where('id', request()->get('order_id'));
+                })
+                ->when(request()->get('order_status_id'), function ($query) {
+                    return $query->whereHas('status', function ($query) {
+                        return $query->where('id', request()->get('order_status_id'));
+                    });
+                });
+
+        return $orders;
     }
 }
